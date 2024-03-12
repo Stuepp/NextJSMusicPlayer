94587cdc66b04cda6eb47d878982391b3969fb7b
Stuepp <arthurh.c@hotmail.com>
2024-03-12 12:09:29 -0300

    making variables

--- src/app/components/PlayingSong.js
+++ src/app/components/PlayingSong.js
@@ -3,10 +3,12 @@ import Image from 'next/image'
 
 export default function PlayingSong(){
     let audioFile = '/audio/O Descobridor Dos Sete Mares.mp3'
+    let imgFile = '/timmaidescobridordossetemares.jpg'
+    let songTitle = 'Descobridor Dos Sete Mares'
     return (
         <div className='flex flex-col items-center text-center my-80'>
-            <Image src='/sample.png' width={110} height={110} alt='Pic of the author' className='rounded-lg border-2 border-rose-500' />
-            <h1 className='text-2xl my-2'>Song Title</h1>
+            <Image src={imgFile} width={110} height={110} alt='Pic of the author' className='rounded-lg border-2 border-black' />
+            <h1 className='text-2xl my-2'>{songTitle}</h1>
             <audio controls>
                 <source src={audioFile} type='audio/mp3' />
             </audio>
